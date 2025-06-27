2fe57fe432614c2a3ca731f91843bdaa3adb30b1
iamhvsharma <itshvsharma@gmail.com>
2025-06-27 23:57:51 +0530

    Update Landing component styling for improved layout

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -6,7 +6,7 @@ import Features from "./Features";
 
 const Landing = () => {
   return (
-    <div>
+    <div className="bg-white text-yellow-metal-900 min-h-screen flex flex-col">
       <Navbar />
       <Hero />
       <Features />
