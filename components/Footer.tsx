a3df55c0b617f2dad99a5fc242153fa0212d3a46
iamhvsharma <itshvsharma@gmail.com>
2025-06-28 00:20:26 +0530

    Refactor Footer component to improve structure and styling

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,24 @@
+import Link from "next/link";
 import React from "react";
 
 const Footer = () => {
-  return <div>
-    <h3>Footer</h3>
-  </div>;
+  return (
+    <div>
+      <footer className="bg-yellow-metal-100 text-yellow-metal-800 py-6 mt-auto">
+        <div className="container mx-auto text-center">
+          <p className="text-sm font-semibold mt-2">
+            Made with ❤️ by Harshvardhan Sharma, Github:{" "}
+            <Link
+              href="https://github.com/iamhvsharma/onevote"
+              className="text-yellow-metal-600 hover:text-yellow-metal-800 transition-colors"
+            >
+              @iamhvsharma
+            </Link>
+          </p>
+        </div>
+      </footer>
+    </div>
+  );
 };
 
 export default Footer;
