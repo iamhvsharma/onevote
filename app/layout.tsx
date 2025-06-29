7307f1decef88aec93c7a3c5077e78fcc33f1a4d
iamhvsharma <itshvsharma@gmail.com>
2025-06-29 19:38:06 +0530

    Remove unused imports from layout and page components for cleaner code

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import { ClerkProvider } from "@clerk/nextjs";
-import Footer from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Landing from "@/components/landing/Landing";
-import Image from "next/image";
 
 
 export default function Home() {
